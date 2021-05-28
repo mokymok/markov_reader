d1e2879e2361b061526e7eccfa6f184c4a83bd6c
mokymok <mebowersock@gmail.com>
2021-05-28 17:38:39 -0500

    complete rewrite yay

--- main.py
+++ main.py
@@ -1,98 +1,52 @@
-import asyncio
-from configparser import ConfigParser
-from os import listdir
-from time import monotonic
-
-import asyncgTTS
-import asyncpg
+import pyttsx3
 import discord
-from aiohttp import ClientSession
+from discord import Intents
+import asyncio
 from discord.ext import commands
+from discord import FFmpegPCMAudio
+from discord.utils import get
 
-from utils import basic, cache, settings
-
-print("Starting TTS Bot!")
-
-start_time = monotonic()
-config = ConfigParser()
-config.read("config.ini")
-t = config["Main"]["Token"]
-config_channels = config["Channels"]
-
-cache_key_str = config["Main"]["key"][2:-1]
-cache_key_bytes = cache_key_str.encode()
-
-# Define bot and remove overwritten commands
-activity = discord.Activity(name=config["Activity"]["name"], type=getattr(discord.ActivityType, config["Activity"]["type"]))
-intents = discord.Intents(voice_states=True, messages=True, guilds=True, members=True)
-status = getattr(discord.Status, config["Activity"]["status"])
-
-
-async def prefix(bot: commands.AutoShardedBot, message: discord.Message) -> str:
-    "Gets the prefix for a guild based on the passed message object"
-    return await bot.settings.get(message.guild, "prefix") if message.guild else "-"
-
-
-bot = commands.AutoShardedBot(
-    status=status,
-    intents=intents,
-    help_command=None, # Replaced by FancyHelpCommand by FancyHelpCommandCog
-    activity=activity,
-    command_prefix=prefix,
-    case_insensitive=True,
-    chunk_guilds_at_startup=False,
-    allowed_mentions=discord.AllowedMentions(everyone=False, roles=False)
-)
-
-bot.session = ClientSession()
-bot.gtts = bot.loop.run_until_complete(
-    asyncgTTS.setup(
-        premium=False,
-        session=bot.session
-    )
-)
-
-pool = bot.loop.run_until_complete(
-    asyncpg.create_pool(
-        host=config["PostgreSQL Info"]["ip"],
-        user=config["PostgreSQL Info"]["name"],
-        database=config["PostgreSQL Info"]["db"],
-        password=config["PostgreSQL Info"]["pass"]
-    )
-)
-
-
-
-
-for cog in listdir("cogs"):
-    if cog.endswith(".py"):
-        bot.load_extension(f"cogs.{cog[:-3]}")
-        print(f"Successfully loaded: {cog}")
-
+intents = Intents.all()
+bot = commands.Bot(intents = intents, command_prefix = '^')
+engine = pyttsx3.init()
+token = 'ODMwMjQyMDc4Mjk4MjEwMzc1.YHD1MA.JoKFmispQ48w01ZN5xzA0DZxOi0'
+loop = asyncio.get_event_loop()
+print(loop)
+self = discord.User
 
 @bot.event
 async def on_ready():
-    await bot.wait_until_ready()
+  print('Logged on as', self)
+  await bot.change_presence(
+      activity = discord.Activity(
+          type = discord.ActivityType.listening, 
+          name = 'Markov'))
 
-    support_server_id = int(config["Main"]["main_server"])
-    bot.supportserver = bot.get_guild(support_server_id)
+@bot.listen('on_message')
+async def tts(ctx):
+  messageContent = ctx.content
+  if ctx.author.bot == True:
+    print(messageContent)
+    engine.save_to_file(messageContent, 'filler.mp3')
+    engine.runAndWait()
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    source = FFmpegPCMAudio('filler.mp3')
+    player = voice.play(source)
+  else:
+    return
 
-    while bot.supportserver is None:
-        print("Waiting 5 seconds")
-        await asyncio.sleep(5)
-        bot.supportserver = bot.get_guild(support_server_id)
+@bot.command(pass_context=True)
+async def join(ctx):
+  channel = ctx.author.voice.channel
+  await channel.connect()
 
-    for channel_name in config_channels:
-        channel_id = int(config_channels[channel_name])
-        channel_object = bot.supportserver.get_channel(channel_id)
-        bot.channels[channel_name] = channel_object
+@bot.command(pass_context=True)
+async def leave(ctx):
+  await ctx.voice_client.disconnect()
 
-    try:
-        await bot.starting_message.edit(content=f"~~{bot.starting_message.content}~~")
-        bot.starting_message = await bot.channels["logs"].send(f"Restarted as {bot.user.name}!")
-    except AttributeError:
-        print(f"Logged into Discord as {bot.user.name}!")
-        bot.starting_message = await bot.channels["logs"].send(f"Started and ready! Took `{monotonic() - start_time:.2f} seconds`")
+@bot.command(pass_context=True)
+async def test(ctx):
+  await ctx.send('This is a test message!', tts=True)
 
-print("\nLogging into Discord...")
-bot.run(t)
+if loop.is_running() == False:
+    bot.run(token)
